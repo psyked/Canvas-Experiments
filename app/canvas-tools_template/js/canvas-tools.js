cfc6af7f80101970eee8cbd814a684f512435e55
James Ford <james.f@mmtdigital.co.uk>
2014-02-08 00:31:10 +0000

    Updated to integrate with colour thief

--- app/canvas-tools_template/js/canvas-tools.js
+++ app/canvas-tools_template/js/canvas-tools.js
@@ -1,28 +1,50 @@
-var canvas-tools = (function() {
+var canvasTools = (function () {
 
-	'use strict';
-	var privateVariable = 'canvas-tools app fired!',
-		docElem = document.documentElement;
+    'use strict';
+//    var privateVariable = 'canvasTools app fired!',
+//        docElem = document.documentElement;
+    var loadFunction = function () {
+//        console.log('on load');
+        var image = $('#largeImage')[0];
+        var colorThief = new ColorThief();
+        var colorCount = 8;
+        var keyColor = colorThief.getColor(image);
+        var color = colorThief.getPalette(image, colorCount, 255);
+//        console.log(color.length);
+        var colors = keyColor.concat(color);
+        for (var i = 0, l = colors.length; i < l; i++) {
+            $('<div></div>').css({
+                width: (100 / colorCount) + '%',
+//                width: 'auto',
+                height: 64,
+                background: 'rgb(' + colors[i][0] + ',' + colors[i][1] + ',' + colors[i][2] + ')',
+                float: 'left'
+            }).insertAfter('#largeImage');
+        }
+    };
 
-	return {
-		publicFunction: function() {
-			console.log(privateVariable);
-		},
-		userAgentInit: function() {
-			docElem.setAttribute('data-useragent', navigator.userAgent);
-		}
-	};
+    return {
+        init: function () {
+//            console.log('init');
+            $('#largeImage').on('load', loadFunction);
+            loadFunction();
+//        },
+//        userAgentInit: function () {
+//            docElem.setAttribute('data-useragent', navigator.userAgent);
+        }
+    };
 
 })();
 
-(function() {
+//(function () {
+$(document).ready(function () {
 
-	'use strict';
+    'use strict';
 
-	//foundation init
-	$(document).foundation();
+    //foundation init
+    $(document).foundation();
 
-	canvas-tools.publicFunction();
-	canvas-tools.userAgentInit();
+    canvasTools.init();
+//    canvasTools.userAgentInit();
 
-})();
+});
